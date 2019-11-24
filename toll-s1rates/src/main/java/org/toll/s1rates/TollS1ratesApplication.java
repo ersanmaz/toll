a3d7fdcb88d84d49858ecb1c1499e819b906c979
Ersan Yilmaz <ersanmaz@gmail.com>
2019-11-24 21:45:36 +0100

    Station1 toll rates with basic security enabled.

---
+++ toll-s1rates/src/main/java/org/toll/s1rates/TollS1ratesApplication.java
@@ -0,0 +1,13 @@
+package org.toll.s1rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollS1ratesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollS1ratesApplication.class, args);
+    }
+
+}
---
+++ toll-s1rates/src/test/java/org/toll/s1rates/TollS1ratesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toll.s1rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollS1ratesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

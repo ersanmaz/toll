0bb39de862287d68e1973e6765401b4eccf7d039
Ersan Yilmaz <ersanmaz@gmail.com>
2019-12-08 18:23:02 +0100

    Creating task intake project.

---
+++ toll-task-intake/src/main/java/org/toll/TollTaskIntakeApplication.java
@@ -0,0 +1,13 @@
+package org.toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollTaskIntakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollTaskIntakeApplication.class, args);
+    }
+
+}

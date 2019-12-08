d297213d6a54e76abf66be91c27d21d892890dfd
Ersan Yilmaz <ersanmaz@gmail.com>
2019-12-08 18:09:33 +0100

    Creating toll task sink

---
+++ toll-task-sink/src/main/java/org/toll/TollTaskSinkApplication.java
@@ -0,0 +1,13 @@
+package org.toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollTaskSinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollTaskSinkApplication.class, args);
+    }
+
+}
